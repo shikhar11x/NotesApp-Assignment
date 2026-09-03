f311ed3eb9b720728200d2fc92633e52b119b826
Shikhar bajpai <142872564+shikhar-blaziee@users.noreply.github.com>
2026-09-03 16:13:17 +0530

    Add navigation between List and Add Note screens

---
+++ lib/main.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+import 'package:notes_app/screens/notes_list_screen.dart';
+import 'package:notesapp/screens/notes_list_screen.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Notes App',
+      theme: ThemeData(
+        
+      ),
+      home: const NotesListScreen(),
+      debugShowCheckedModeBanner: false,
+    );
+  }
+}
